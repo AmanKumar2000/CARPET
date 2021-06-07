57c76680c15fb64685b1aee93a7a885e78a82e67
Aman Kumar <amankumar2472000@gmail.com>
2021-06-07 21:19:01 +0530

    carpet1.0.0

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+
+
+app.get("/", function(req, res) {
+
+  res.sendFile(__dirname+"\\index.html", {});
+
+});
+
+
+
+app.listen(process.env.PORT||5000, function() {
+  console.log("the server is running on port 3000");
+});
